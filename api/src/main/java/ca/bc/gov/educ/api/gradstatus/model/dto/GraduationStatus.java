ea860c96f571c250f0b1e00f923f6567855e605c
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-01-19 15:08:14 -0800

    Change in student grad status table

--- api/src/main/java/ca/bc/gov/educ/api/gradstatus/model/dto/GraduationStatus.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstatus/model/dto/GraduationStatus.java
@@ -16,22 +16,15 @@ public class GraduationStatus extends BaseModel{
 
     private StringBuffer studentGradData;
     private String pen;
-    private String gradProgram;
-    private String graduationDate; 
-    private String sccpGraduationDate;
-    private String studentGradeAtGraduation;
+    private String program;
+    private String programCompletionDate;
     private String gpa;
     private String honoursFlag;
     private String certificateType1;
     private String certificateType2;
     private String certificateType1Date; 
     private String certificateType2Date; 
-    private String frenchProgramParticipation;
-    private String advancePlacementParticipation;    
-    private String careerProgramParticipation;
-    private String recalculateFlag;
-    private String ibParticipationFlag;    
-    private String transcriptDate;  
+    private String recalculateFlag;   
     private String schoolOfRecord;
     private String studentGrade;	
 				
