83bc7c186b1aab2dffb67e336fe91c9e7759ee52
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-04-12 12:59:30 -0700

    Documentation version change and new endpoint

--- api/src/main/java/ca/bc/gov/educ/api/gradstatus/EducGradStatusApiApplication.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstatus/EducGradStatusApiApplication.java
@@ -6,6 +6,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.client.RestTemplate;
 
 import ca.bc.gov.educ.api.gradstatus.model.dto.GraduationStatus;
@@ -36,5 +40,24 @@ public class EducGradStatusApiApplication {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+	
+	@Configuration
+	static
+	class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+	  /**
+	   * Instantiates a new Web security configuration.
+	   * This makes sure that security context is propagated to async threads as well.
+	   */
+	  public WebSecurityConfiguration() {
+	    super();
+	    SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+	  }
+	  @Override
+	  public void configure(WebSecurity web) {
+	    web.ignoring().antMatchers("/v3/api-docs/**",
+	            "/actuator/health","/actuator/prometheus",
+	            "/swagger-ui/**", "/health");
+	  }
 	}
 }
