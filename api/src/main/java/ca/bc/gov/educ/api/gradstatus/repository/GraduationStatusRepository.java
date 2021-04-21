13fe24298a61d18c3b75e18919df14a275381489
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-04-21 13:49:19 -0700

    Student Status Changes

--- api/src/main/java/ca/bc/gov/educ/api/gradstatus/repository/GraduationStatusRepository.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradstatus/repository/GraduationStatusRepository.java
@@ -3,6 +3,7 @@ package ca.bc.gov.educ.api.gradstatus.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ca.bc.gov.educ.api.gradstatus.model.entity.GraduationStatusEntity;
@@ -13,4 +14,7 @@ public interface GraduationStatusRepository extends JpaRepository<GraduationStat
     List<GraduationStatusEntity> findAll();
 
 	List<GraduationStatusEntity> findByRecalculateGradStatus(String recalulateFlag);
+
+	@Query("select c from GraduationStatusEntity c where c.studentStatus=:statusCode")
+	List<GraduationStatusEntity> existsByStatusCode(String statusCode);
 }
